f79dfaa5c186326059d87317e29490abb3793780
MiCetF <webmaster@micetf.fr>
2022-03-13 15:19:15 +0100

    Modif config webpack : images dans dossier img en prod

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     output: {
         filename: "js/[name].js",
         path: path.resolve(__dirname, "dist"),
-
+        assetModuleFilename: "img/[name][ext]",
         clean: true,
     },
 
